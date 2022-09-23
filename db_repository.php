75313dd8c873e33cacce90b644d5777432f100a7
koenti91 <koentiepel@live.nl>
2022-09-23 17:02:30 +0200

    01. Opdracht 3.1 - Introductie SQL #2

--- db_repository.php
+++ db_repository.php
@@ -19,14 +19,36 @@ function closeDatabase($conn) {
     mysqli_close($conn);
 }
 
-function runQuery($sql) {
+function runQuery($sql, $types='', $params=array()) {
     $conn = connectDatabase();
 
-    $result = mysqli_query($conn, $sql);
+    $stmt = mysqli_prepare($conn, $sql);
+    if(!empty ($params)) {
+    mysqli_stmt_bind_param($stmt, $types, $params);
+    }
+
+    mysqli_stmt_execute($stmt);
+    $metaResults = mysqli_stmt_result_metadata($stmt);
+    $fields = mysqli_fetch_fields($metaResults);
+    $statementParams='';
+    $column = array();
+    //build the bind_results statement dynamically so I can get the results in an array
+    foreach($fields as $field){
+        if(empty($statementParams)){
+            $statementParams.="\$column['".$field->name."']";
+        }else{
+            $statementParams.=", \$column['".$field->name."']";
+        }
+    }
+    $statement="mysqli_stmt_bind_result(\$stmt, $statementParams);";
+    var_dump($statement);
+    eval($statement);
     $query_result = [];
 
-    if (mysqli_num_rows($result) > 0) {
-      while($row = mysqli_fetch_assoc($result)) {
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+      while(mysqli_stmt_fetch($stmt)) {
+        $row = array ($column);
+        var_dump($row);
         $query_result[] = $row;
       }
     }
@@ -40,18 +62,18 @@ function findAll($sql) {
   return runQuery($sql);
 }
 
-function findOne($sql) {
-  $result =  runQuery($sql);
+function findOne($sql, $types, $params) {
+  $result =  runQuery($sql, $types, $params);
 
   if (!empty($result[0])) return $result[0];
 }
 
 function findUserByEmail($email) {
-  return findOne("SELECT * FROM users WHERE email = '$email'");
+  return findOne("SELECT * FROM users WHERE email = ?", "s", array($email));
 }
 
 function findUserByID($userId){
-    return findOne("SELECT * FROM users WHERE id = '$userId'");
+    return findOne("SELECT * FROM users WHERE id = ?", "i", array($userId));
 }
 
 function saveUser($name, $email, $password) {
